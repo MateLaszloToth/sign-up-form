8cd2d5a3b6558f98a01187e6adaa5bc549d00d8c
Mate Toth <mate.toth@kambr.com>
2022-12-29 19:07:45 +0100

    feat: Add password validation and fix up style

--- control.js
+++ control.js
@@ -1,1 +1,29 @@
-console.log("I'm alive");
+const passwordFields = document.querySelectorAll("input[type=password]");
+
+function comparePasswords(e) {
+    const isCorrectPwd = passwordFields[0].value === passwordFields[1].value
+    const errorElement = passwordFields[1].parentElement.querySelector('p');
+    if (!isCorrectPwd) {
+        if (errorElement === null) {
+            const newErrorElement = document.createElement("p");
+            newErrorElement.textContent = "Passwords must be identical";
+            newErrorElement.style.position = "absolute";
+            newErrorElement.style.left = "7px";
+            newErrorElement.style.bottom = "-20px";
+            newErrorElement.style.fontSize = "12px";
+            newErrorElement.style.color = "coral";
+            passwordFields[1].parentElement.appendChild(newErrorElement);
+        }
+    } else {
+        if (errorElement !== null)
+            errorElement.remove();
+    }
+}
+
+function assignEventListeners() {
+    for (let field of passwordFields) {
+        field.addEventListener('change', comparePasswords);
+    }
+}
+
+assignEventListeners();
